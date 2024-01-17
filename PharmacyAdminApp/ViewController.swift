0521458edf82ffefefa64755fcba28e25f15e224
ShashiSewwandi <80105028+ShashiSewwandi@users.noreply.github.com>
2024-01-17 14:25:37 +0530

    Initial Commit

---
+++ PharmacyAdminApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PharmacyAdminApp
+//
+//  Created by Sashikala Sewwandi on 2024-01-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PharmacyAdminAppUITests/PharmacyAdminAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PharmacyAdminAppUITestsLaunchTests.swift
+//  PharmacyAdminAppUITests
+//
+//  Created by Sashikala Sewwandi on 2024-01-17.
+//
+
+import XCTest
+
+final class PharmacyAdminAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
