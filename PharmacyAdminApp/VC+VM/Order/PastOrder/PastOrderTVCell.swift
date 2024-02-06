d3ba63c0fdbd4e82e4ca47e668f5689673a13479
SSGITACCOUNT <67000290+SSGITACCOUNT@users.noreply.github.com>
2024-02-06 10:21:47 +0530

    Order View Controller

--- PharmacyAdminApp/VC+VM/Order/PastOrder/PastOrderTVCell.swift
+++ PharmacyAdminApp/VC+VM/Order/PastOrder/PastOrderTVCell.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class PastOrderTVCell: UITableViewCell {
 
+    @IBOutlet weak var logoNameImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var orderNoLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var statusLbl: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
