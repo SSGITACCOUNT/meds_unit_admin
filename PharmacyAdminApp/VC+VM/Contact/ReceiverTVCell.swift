9c89960ccefe3ecdb1d88a7b2b3f204f353e28bf
SSGITACCOUNT <67000290+SSGITACCOUNT@users.noreply.github.com>
2024-02-11 10:05:57 +0530

    chat view controller

---
+++ PharmacyAdminApp/VC+VM/Contact/ReceiverTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReceiverTVCell.swift
+//  PharmacyAdminApp
+//
+//  Created by Shashee on 2024-02-08.
+//
+
+import UIKit
+
+class ReceiverTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ PharmacyAdminApp/VC+VM/Contact/SenderTVCell.swift
@@ -0,0 +1,23 @@
+//
+//  SenderTVCell.swift
+//  PharmacyAdminApp
+//
+//  Created by Shashee on 2024-02-08.
+//
+
+import UIKit
+
+class SenderTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
