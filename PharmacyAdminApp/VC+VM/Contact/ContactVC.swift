6181ec27a80a5f9217a1a53da4290779b997bc5c
SSGITACCOUNT <67000290+SSGITACCOUNT@users.noreply.github.com>
2024-02-28 22:47:44 +0530

    Chat UI

--- PharmacyAdminApp/VC+VM/Contact/ContactVC.swift
+++ PharmacyAdminApp/VC+VM/Contact/ContactVC.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class ContactVC: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    let vm = ContactVM()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.register(UINib(nibName: "SenderTVCell", bundle: nil), forCellReuseIdentifier: "SenderTVCell")
+        tableview.register(UINib(nibName: "ReceiverTVCell", bundle: nil), forCellReuseIdentifier: "ReceiverTVCell")
+        
     }
-
 }
+
+extension ContactVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SenderTVCell", for: indexPath)
+        return cell
+    }
+}
+
+
---
+++ PharmacyAdminApp/VC+VM/Contact/ContactVM.swift
@@ -0,0 +1,13 @@
+//
+//  ContactVM.swift
+//  PharmacyAdminApp
+//
+//  Created by Shashee on 2024-02-28.
+//
+
+import Foundation
+
+class ContactVM {
+    
+}
+
